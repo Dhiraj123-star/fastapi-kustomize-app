c79f5a8f86ae80e3152402eeecfafeaea7e6c765
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-12-12 12:55:35 +0530

    feat: Add /healthz endpoint for Kubernetes probes

--- app/main.py
+++ app/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 def read_root():
     return {"message":"Hello from FastAPI + Docker + Kustomize + Github Actions!!"}
 
+@app.get("/healthz")
+def health_check():
+    """
+    Kubernetes Readiness and Liveness Probe endpoint.
+    Returns 200 OK if the application is running.
+    """
+    return {"status":"Ok"}
